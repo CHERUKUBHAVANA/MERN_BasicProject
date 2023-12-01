7b363cc392861682fbe1dc071b08f71195d36103
CHERUKU BHAVANA <98461778+CHERUKUBHAVANA@users.noreply.github.com>
2023-12-01 19:54:44 +0530

    Production ready

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ const App = () => {
         <h1 className='p-5'>React Node MongoDB Authentication Boilerplate</h1>
         <h2>MERN STACK</h2>
         <hr/>
-        <p className='lead'>MERN Stack is a JavaScript Stack that is used for easier and faster deployment of full-stack web applications. MERN Stack comprises of 4 technologies namely: MongoDB, Express, React and Node.js. It is designed to make the development process smoother and easier.</p>
+        <p className='lead'>MERN stack login register system with account activation, forgot password, reset password, login with google as well as private and protected routes for authenticated user and users with the role of admin.</p>
+        <h2>Bonus</h2>
+        <p className='lead'>Profile Update & deployment to digital ocean cloud servers.</p>
       </div>
     </Layout>
   )
